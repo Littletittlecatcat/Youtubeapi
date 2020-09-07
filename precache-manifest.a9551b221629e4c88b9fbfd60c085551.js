7995a86a5fca89468c0e311e34ab14ed00d1ac2e
Littletittlecatcat <yw.chen.michael@gmail.com>
2020-09-07 14:01:42 +0800

    Updates

---
+++ precache-manifest.a9551b221629e4c88b9fbfd60c085551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8e21ee5bae584d2c2cdc175d181a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67999294f4a6717f9d90",
+    "url": "/static/css/main.b2318bab.chunk.css"
+  },
+  {
+    "revision": "51acb90743efe978845f",
+    "url": "/static/js/2.816889d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.816889d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67999294f4a6717f9d90",
+    "url": "/static/js/main.04c6d9e9.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
